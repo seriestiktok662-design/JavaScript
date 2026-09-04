c72a4dba8a683bba89d7d7fd3c150a0eb7bf998e
seriestiktok662-design <seriestiktok662@gmail.com>
2026-09-04 17:01:31 -0300

    declaracao-var.js

---
+++ declaracao-var.js
@@ -0,0 +1,10 @@
+function exemploVar() {
+    var x = 10;
+    if (true) {
+        var x = 20; // mesma variável x é redeclarada dentro do bloco
+        console.log(x); // 20
+    }
+    console.log(x); // 20 (o valor foi alterado)
+}
+
+exemploVar();
