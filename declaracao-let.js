8fe802671c5ee8412d654c6c332945759d3d391d
seriestiktok662-design <seriestiktok662@gmail.com>
2026-09-04 16:24:44 -0400

    declaracao-let.js

---
+++ declaracao-let.js
@@ -0,0 +1,10 @@
+function exemploLet() {
+    let x = 10;
+    if (true) {
+        let x = 20; // nova variável x é criada dentro do bloco
+        console.log(x); // 20 (dentro do bloco)
+    }
+    console.log(x); // 10 (fora do bloco)
+}
+
+exemploLet();
