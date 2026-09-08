50ca15d7bbbb563db9170fc8e565863d20cf660a
marina-arruda <seriestiktok662@gmail.com>
2026-09-08 16:19:33 -0400

    add tipo-de-dados-strings

---
+++ tipo-de-dados-strings.js
@@ -0,0 +1,3 @@
+let nome = "Davi"; // String com aspas duplas
+let sobrenome = 'Silva'; // String com aspas simples
+let saudacao = `Olá, ${nome}!`; // Template literal com interpolação de variáveis
