941bc2940f0c739d1c146fd219c04560755c3deb
marina-arruda <seriestiktok662@gmail.com>
2026-09-08 16:20:15 -0400

    add tipo-de-dados-array

---
+++ tipo-de-dados-array.js
@@ -0,0 +1,3 @@
+let frutas = ["maçã", "banana", "laranja"]; // Array de strings
+let numeros = [1, 2, 3, 4, 5]; // Array de números
+let misto = [true, "texto", 42]; // Array com diferentes tipos de dados
