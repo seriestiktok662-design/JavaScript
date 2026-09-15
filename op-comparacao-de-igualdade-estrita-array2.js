358de3e2d81acbcd307cb84c65d0d180f429889b
mariaceciliaalencar21-creator <seriestiktok662@gmail.com>
2026-09-15 14:13:30 -0400

    add op-comparacao-de-igualdade-estrita-array2

---
+++ op-comparacao-de-igualdade-estrita-array2.js
@@ -0,0 +1,10 @@
+// listaA é um array com 5 elementos
+let listaA = [10, 20, 30, 40, 50];
+
+// listaB é outro array com os mesmos 5 elementos que listaA
+let listaB = [10, 20, 30, 40, 50];
+
+// listaC recebe uma copia de listaA
+let listaC = listaA;
+
+console.log(listaA === listaC);
