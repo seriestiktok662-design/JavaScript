51cff9d94fd358cfddb2ff9bbfb8a7cc821faa40
mariana-sesc <seriestiktok662@gmail.com>
2026-09-22 14:09:25 -0400

    add if

---
+++ if.js
@@ -0,0 +1,7 @@
+let hora = 15
+
+if (hora < 12) {
+    console.log("Bom dia!");
+}
+// O restante do código continua independentemente se a condição for verdadeiro ou não
+console.log("Dinheiro não traz felicidade, me dê o seu, e seja feliz!🤪");
