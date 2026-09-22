045276718593c63704ba8b42d1d8654e0288ec75
mariana-sesc <seriestiktok662@gmail.com>
2026-09-22 14:18:33 -0400

    add if-else-2

---
+++ if-else-2.js
@@ -0,0 +1,7 @@
+let diaDaSemana = "quarta-feira";
+
+if (diaDaSemana === "sábado" || diaDaSemana === "domingo") {
+    console.log("É fim de semana!");
+} else {
+    console.log("É dia de semana. ");
+}
